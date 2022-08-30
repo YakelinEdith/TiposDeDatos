68410926b84193407732c2f4c7751fd026d93ca5
YakelinEdith <yakelinedith10@gmail.com>
2022-08-30 10:35:36 -0600

    Primer commit

---
+++ src/familiaconvaraibles/FamiliaConVaraibles.java
@@ -0,0 +1,35 @@
+
+package familiaconvaraibles;
+
+
+public class FamiliaConVaraibles {
+
+    public static void main(String[] args) {
+     int FamiliaIntegrantes=4;
+    System.out.println("Mi familia esta esta conformoda por:");
+    System.out.println(FamiliaIntegrantes+ " integrantes");
+    
+    
+    System.out.println("El primero de ellos es mi papá");
+    String NombrePadre = "Manuel Edilberto Ramirez Mendoza";
+    System.out.println(NombrePadre);
+    int EdadPadre = 47;
+    System.out.println("Él tiene " +EdadPadre+ " años");
+  
+    System.out.println ("La siguiente es mi mamá, su nombre es: ");
+    String NombreMadre = "María Antonia Hernanadéz Gonsaléz";
+    System.out.println(NombreMadre);
+     int EdadMadre = 46;
+      System.out.println("Ella tiene " +EdadMadre+ " años");
+       System.out.println ("Para finalizar tenemos a mi hermano, su nombre es: ");
+       
+    String NombreHermano = "Juan Antonio Ramirez Gonsaléz";
+    System.out.println(NombreHermano);
+     int EdadHermano = 22;
+      System.out.println("Èl tiene " +EdadHermano+ " años");
+    
+            
+            
+    }
+    
+}
